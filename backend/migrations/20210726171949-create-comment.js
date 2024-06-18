8a700afbf3ed0785a7217424e07ad956403f757b
sarap2662 <sarapena7@gmail.com>
2024-06-17 21:50:22 -0700

    updates from assignment

--- backend/migrations/20210726171949-create-comment.js
+++ backend/migrations/20210726171949-create-comment.js
@@ -1,39 +1,49 @@
-'use strict';
+"use strict";
+
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('comments', {
-      comment_id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      place_id: {
-        type: Sequelize.SMALLINT
-      },
-      stars: {
-        type: Sequelize.FLOAT
-      },
-      content: {
-        type: Sequelize.STRING
-      },
-      rant: {
-        type: Sequelize.BOOLEAN
-      },
-      author_id: {
-        type: Sequelize.SMALLINT
-      },
-      created_at: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updated_at: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
+    return queryInterface.addColumn("users", "password_digest", {
+      type: Sequelize.DataTypes.STRING,
     });
   },
+
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('comments');
-  }
+    return queryInterface.removeColumn("users", "password_digest");
+  },
+};
+
+await queryInterface.createTable("comments", {
+  comment_id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: Sequelize.INTEGER,
+  },
+  place_id: {
+    type: Sequelize.SMALLINT,
+  },
+  stars: {
+    type: Sequelize.FLOAT,
+  },
+  content: {
+    type: Sequelize.STRING,
+  },
+  rant: {
+    type: Sequelize.BOOLEAN,
+  },
+  author_id: {
+    type: Sequelize.SMALLINT,
+  },
+  created_at: {
+    allowNull: false,
+    type: Sequelize.DATE,
+  },
+  updated_at: {
+    allowNull: false,
+    type: Sequelize.DATE,
+  },
+});
+
+down: async (queryInterface, Sequelize) => {
+  return queryInterface.removeColumn("users", "password_digest");
 };
--- backend/models/user.js
+++ backend/models/user.js
@@ -20,6 +20,7 @@ module.exports = (sequelize, DataTypes) => {
     firstName: DataTypes.STRING,
     lastName: DataTypes.STRING,
     email: DataTypes.STRING
+    passwordDigest: DataTypes.STRING
   }, {
     sequelize,
     underscored: true,
